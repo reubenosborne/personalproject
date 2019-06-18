e7b5e6fc64e9c8aac195553726fac06d19d2649c
student <kiwdev1984@icloud.com>
2019-06-18 19:28:44 +1200

    Cycle through array with interval

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const clientID =
   "2116bdc2465b1c0744e76fb082d7d7103784b033cb4a6dd20c82667efb023068";
-let searchParams = "photos/random";
-let searchQuery = 'oregon'
+let searchParams = "search/photos";
+let searchQuery = 'blossom'
 
 fetch(
   `https://api.unsplash.com/${searchParams}?query=${searchQuery}&orientation=landscape&client_id=${clientID}`)
   .then(response => response.json())
   .then(json => {
-    let background = json.urls.full;
-    document.body.style.backgroundImage = `url(${background})`;
+
+    setInterval(function changeBackground() {
+      let background = json.results[count].urls.full;
+      document.body.style.backgroundImage = `url(${background})`;
+      if (count == 9) {
+        count = 0
+      } else {
+        count++
+      }
+    }, 10000)
+
   })
+
+let count = 0
