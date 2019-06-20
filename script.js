23261536afb38f36eb16d9229f88b1892412a0ab
Reuben Osborne <10952926+reubenosborne@users.noreply.github.com>
2019-06-20 17:28:43 +1200

    Set interval timer

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const clientID =
   "2116bdc2465b1c0744e76fb082d7d7103784b033cb4a6dd20c82667efb023068";
 let searchParams = "search/photos";
-let searchQuery = 'blossom'
+let searchQuery = 'island'
 
 fetch(
   `https://api.unsplash.com/${searchParams}?query=${searchQuery}&orientation=landscape&client_id=${clientID}`)
   .then(response => response.json())
   .then(json => {
+
+
+    function initialBackground() {
+      let background = json.results[count].urls.full;
+      document.body.style.backgroundImage = `url(${background})`;
+    }
+
+    initialBackground()
 
     setInterval(function changeBackground() {
       let background = json.results[count].urls.full;
@@ -16,8 +24,19 @@ fetch(
       } else {
         count++
       }
-    }, 10000)
+    }, 5000)
 
   })
 
 let count = 0
+
+// window.localStorage.getItem()
+// Geo location (HTML5) 
+// Weather
+// Settings UI
+// Two api calls
+// Json stringify
+// React
+// Look at react components
+
+// Changes > Build in a chrome extension
