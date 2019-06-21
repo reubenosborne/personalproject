227f4d3d1f92b4ef969bf8dc3424123a1a5becdd
Reuben Osborne <10952926+reubenosborne@users.noreply.github.com>
2019-06-21 18:04:35 +1200

    Add localstorage

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const clientID =
   "2116bdc2465b1c0744e76fb082d7d7103784b033cb4a6dd20c82667efb023068";
 let searchParams = "search/photos";
-let searchQuery = 'island'
+let searchQuery = 'whippet'
+
+// Set Query
+localStorage.setItem('query', searchQuery)
 
 fetch(
   `https://api.unsplash.com/${searchParams}?query=${searchQuery}&orientation=landscape&client_id=${clientID}`)
