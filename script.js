81b50fffe8444f9e962f8e6911ee19a9ded93853
Reuben Osborne <reuben.osborne@gmail.com>
2019-07-05 01:16:38 +1200

    Fix white bg bug

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 const clientID =
   "2116bdc2465b1c0744e76fb082d7d7103784b033cb4a6dd20c82667efb023068";
 let searchParams = "search/photos";
-let searchQuery = 'whippet'
+let searchQuery = "vancouver";
 
 // Set Query
-localStorage.setItem('query', searchQuery)
+localStorage.setItem("query", searchQuery);
+
+// Set Count
+let count = 0;
 
 fetch(
-  `https://api.unsplash.com/${searchParams}?query=${searchQuery}&orientation=landscape&client_id=${clientID}`)
+  `https://api.unsplash.com/${searchParams}?query=${searchQuery}&orientation=landscape&client_id=${clientID}`
+)
   .then(response => response.json())
   .then(json => {
-
-    console.log(json)
+    console.log(json);
 
     function initialBackground() {
-      let background = json.results[count].urls.full;
-      document.body.style.backgroundImage = `url(${background})`;
+      let background = json.results[count].urls.regular;
+      document.body.style.backgroundImage = `url(${background})`
     }
 
-    initialBackground()
+    initialBackground();
 
     setInterval(function changeBackground() {
       let background = json.results[count].urls.full;
-      document.body.style.backgroundImage = `url(${background})`;
+      document.body.style.backgroundImage = `url(${background})`
       if (count == 9) {
-        count = 0
+        count = 0;
       } else {
-        count++
+        count++;
       }
-    }, 2000)
+    }, 4500);
+  });
 
-  })
-
-let count = 0
 
 // window.localStorage.getItem()
-// Geo location (HTML5) 
+// Geo location (HTML5)
 // Weather
 // Settings UI
 // Two api calls
